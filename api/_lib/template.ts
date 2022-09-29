cc7cd70b9ee1c846f7c62dc66f776ccbd9c4a14b
Peter Kim <p@peter-kim.com>
2022-09-29 12:40:34 -0700

    updating og-image template

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -7,6 +7,7 @@ const emojify = (text: string) => twemoji.parse(text, twOptions);
 
 const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
 const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
+const bungee = readFileSync(`${__dirname}/../_fonts/Bungee-Regular.woff2`).toString('base64');
 
 function getCss() {
     return `
@@ -24,159 +25,23 @@ function getCss() {
         src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
     }
 
+    @font-face {
+        font-family: 'Bungee';
+        font-style:  normal;
+        font-weight: bold;
+        src: url(data:font/woff2;charset=utf-8;base64,${bungee}) format('woff2');
+    }
+
+    html {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
     body {
         width: 100vw;
         height: 100vh;
         font-family: 'Inter', sans-serif;
     }
-
-    .logo-wrapper {
-        display: flex;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        justify-items: center;
-    }
-
-    .logo {
-        margin: 0 75px;
-    }
-
-    .heading {
-        font-size: 100px;
-        font-style: normal;
-        color: black;
-        line-height: 1.8;
-    }
-
-    .header {
-        display: flex;
-        align-items: center;
-        padding-left: 80px;
-        padding-right: 140px;
-        margin-top: 50px;
-        justify-content: space-between;
-    }
-
-    .header-logo {
-        display: flex;
-        align-items: center;
-    }
-
-    .btn {
-        font-size: 60px;
-        padding: 10px 20px;
-        border-color: black;
-        border-width: 5px;
-        border-style: solid;
-        border-radius: 20px;
-        box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
-    }
-
-    .body {
-        font-family: 'Inter', sans-serif;
-        padding: 50px 50px;
-        margin: 0 80px;
-        border: 0;
-        border-color: #F0F0F0;
-        border-width: 4px;
-        border-style: solid;
-        position: relative;
-    }
-
-    .excerpt {
-        font-size: 40px;
-        line-height: 60px;
-        white-space: pre-wrap;
-        display: -webkit-box;
-        -webkit-line-clamp: 7;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-    }
-
-    .about-author-text {
-        width: 100%;
-    }
-
-    .about-author {
-        display: flex;
-        align-items: center;
-        margin-bottom: 50px;
-        width: 100%;
-    }
-
-    .about-author-img {
-        width: 150px;
-        border-radius: 100px;
-        margin-right: 20px;
-    }
-
-    .author-name {
-        font-size: 50px;
-        max-width: 60%;
-        font-weight: 700;
-    }
-
-    .author-username {
-        font-size: 40px;
-        color: #A6A6A6;
-    }
-
-    .emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
-    }
-
-    .perl-points {
-        display: flex;
-        align-items: center;
-        justify-self: center;
-        margin-bottom: 40px;
-    }
-
-    .perl-points-number {
-        margin-left: 10px;
-        font-size: 32px;
-    }
-
-    .perl-points-img {
-        width: 32px;
-    }
-
-    .body-header {
-        display: flex;
-        justify-content: space-between;
-    }
-
-    .farcaster-img {
-        width: 32px;
-    }
-
-    .via {
-        display: flex;
-        align-items: center;
-        width: 290px;
-        align-self: start;
-        margin-top: 20px;
-    }
-
-    .via-text {
-        font-size: 32px;
-        margin-right: 16px;
-    }
-
-    .clam-anchor-img {
-        width: 300px;
-    }
-
-    .clam-anchor-container {
-        position: absolute;
-        bottom: 0;
-        right: 0;
-    }
-
     `;
 }
 
@@ -199,45 +64,98 @@ export function getHtml(parsedReq: ParsedRequest) {
     <style>
         ${getCss()}
     </style>
-    <body>
-        <div class="header">
-            <div class="about-author">
-                <img class="about-author-img" src="${perlerImage}"></img>
-                <div class="about-author-text">
-                    <div class="author-name">
-                        ${emojify(perlerName.toString())}
-                    </div>
-                    <div class="author-username">
-                        @${perlerUsername}
-                    </div>
-                </div>
-            </div>
-            <div class="perl-points">
-                <img class="perl-points-img" src="https://storage.googleapis.com/moon-lab/perl.png"></img>
-                <div class="perl-points-number">${perlPoints}</div>
-            </div>
-        </div>
-        <div class="body">
-            <div class="body-header">
-                <div class="about-author">
-                    <img class="about-author-img" src="${authorImage}"></img>
-                    <div class="about-author-text">
-                        <div class="author-name">
-                            ${emojify(authorName.toString())}
-                        </div>
-                        <div class="author-username">
-                            @${authorUsername}
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+        theme: {
+            extend: {
+                fontFamily: {
+                    'bungee': ['Bungee'],
+                },
+                colors: {
+                    'perl-purple': '#9059C4',
+                    'perl-gray': '#F2F2F2',
+                }
+            }
+        }
+        }
+    </script>
+    <body class="bg-cover bg-[url('https://storage.googleapis.com/moon-lab/perl-bg.png')]">
+        <div class="grid px-12 py-8 grid-cols-2 grid-rows-1 gap-x-8">
+            <div class="bg-white w-[100%] h-[90vh] p-4">
+                <div class="flex items-start justify-between mb-4">
+                    <div class="flex items-center">
+                        <img class="rounded-full object-cover h-[55px] w-[55px] mr-2" src="https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_png,w_256/https://img.seadn.io/files/06496571b614d3a4b424f2b1f2402a8f.png?h=1024&w=1024&auto=format"/>
+                        <div>
+                            <div class="text-xl font-bold">Peter Kim</div>
+                            <div class="opacity-60">@peter</div>
                         </div>
                     </div>
+                    <div>
+                        <div class="flex items-center mt-1">
+                            <Image width="14px" height="14px" src="https://storage.googleapis.com/moon-lab/perl-small.png" />
+                            <div class="ml-2 mr-6">23</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="via">
-                    <div class="via-text">via Farcaster</div>
-                    <img class="farcaster-img" src="https://storage.googleapis.com/moon-lab/farcaster-logo.png"></img>
+                <div class="mb-6">
+                    web3 founder fellow @ South Park Commons | Previously SBLY.com, Stripe | peter-kim.com
+                </div>
+                <div class="mb-4 bg-perl-gray py-2 px-4 mr-5 flex items-center w-full">
+                    <Image width="13px" height="13px" src="https://storage.googleapis.com/moon-lab/search.png"/>
+                    <input id="search" class="bg-perl-gray ml-2 w-full" placeholder="Search Peter Kim's perls"></input>
+                </div>
+                <div class="border-2 border-perl-gray p-4">
+                    <div class="flex justify-between items-center">
+                        <div class="flex items-center">
+                            <img class="rounded-full w-[55px] h-[55px] object-cover" src="https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_png,w_256/https://img.seadn.io/files/06496571b614d3a4b424f2b1f2402a8f.png?h=1024&w=1024&auto=format"/>
+                            <div class="ml-2">
+                                <div class="font-bold">Peter Kim</div>
+                                <div class="opacity-60">@peter</div>
+                            </div>
+                        </div>
+                        <div class="flex items-center">
+                            <div class="mr-2 text-xs sm:text-sm opacity-80">via Farcaster</div>
+                                <Image width="14px" height="14px" src="https://storage.googleapis.com/moon-lab/farcaster.png" />
+                            </div>
+                        </div>
+                        <div class="mb-4 overflow-clip whitespace-pre-line break-words">
+                            i personally was very impressed by Evan Spiegel’s vision for Snap in just a few words.
+
+                            “Snap is a camera company.”
+
+                            you open the app, and guess what you see - a camera.
+
+                            he told it with his persona & product at the same time
+                        </div>
                 </div>
             </div>
-            <div class="excerpt">${emojify(excerpt.toString())}</div>
-            <div class="clam-anchor-container">
-                <img class="clam-anchor-img" src="https://storage.googleapis.com/moon-lab/perl-clam.png"></img>
+            <div>
+                <div class="w-[540px] h-[35vh]">
+                    <div class="w-[540px] h-[50px] bg-perl-purple px-4 flex items-center">
+                        <div class="font-bungee uppercase text-white text-3xl flex items-center">Collected By</div>
+                    </div>
+                    <div class="flex gap-x-4 w-full items-center py-6">
+                        <div class="flex gap-x-4 w-[90px] h-[90px] items-center justify-center rounded-full bg-white">
+                            <img class="rounded-full w-[80px] h-[80px]" src="https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_png,w_256/https://img.seadn.io/files/06496571b614d3a4b424f2b1f2402a8f.png?h=1024&w=1024&auto=format"></img>
+                        </div>
+                        <div class="flex gap-x-4 w-[90px] h-[90px] items-center justify-center rounded-full bg-white">
+                            <img class="rounded-full w-[80px] h-[80px]" src="https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_png,w_256/https://img.seadn.io/files/06496571b614d3a4b424f2b1f2402a8f.png?h=1024&w=1024&auto=format"></img>
+                        </div>
+                        <div class="flex gap-x-4 w-[90px] h-[90px] items-center justify-center rounded-full bg-white">
+                            <img class="rounded-full w-[80px] h-[80px]" src="https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_png,w_256/https://img.seadn.io/files/06496571b614d3a4b424f2b1f2402a8f.png?h=1024&w=1024&auto=format"></img>
+                        </div>
+                        <div class="flex gap-x-4 w-[90px] h-[90px] items-center justify-center rounded-full bg-white">
+                            <img class="rounded-full w-[80px] h-[80px]" src="https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_png,w_256/https://img.seadn.io/files/06496571b614d3a4b424f2b1f2402a8f.png?h=1024&w=1024&auto=format"></img>
+                        </div>
+                        <div class="font-bungee text-3xl">+42</div>
+                    </div>
+                </div>
+                <div class="w-[540px] h-[50vh]">
+                    <div class="w-[540px] h-[50px] bg-perl-purple px-4 flex items-center">
+                        <div class="font-bungee uppercase text-white text-3xl flex items-center">Badges</div>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
